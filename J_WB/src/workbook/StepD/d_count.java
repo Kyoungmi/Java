19f45fb3e97a0859618535fcdc646f2dd751c5f5
kkongmi <kkongmi@192.168.0.107>
2018-12-27 23:02:20 +0900

    StepD modified

--- J_WB/src/workbook/StepD/d_count.java
+++ J_WB/src/workbook/StepD/d_count.java
@@ -5,8 +5,6 @@ import java.util.Scanner;
 public class d_count {
 	private int count_all;
 	private int count_young=0;
-	private int birth_year;
-	private int age, i;
 	
 	public d_count() { input(); }
 
@@ -16,10 +14,10 @@ public class d_count {
 		System.out.print("가족이 몇 명인지 입력하세요 ");
 		this.count_all = s.nextInt();
 		
-		for(i=0;i<count_all; i++){
+		for(int i=0;i<count_all; i++){
 			System.out.print("태어난 년도를 입력하세요 ");
-			this.birth_year=s.nextInt();
-			this.age = 2018 - birth_year +1;
+			int birth_year = s.nextInt();
+			int age = 2018 - birth_year +1;
 			if(age<20) this.count_young = count_young +1;
 		}
 	
