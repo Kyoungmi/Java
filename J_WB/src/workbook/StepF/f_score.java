d9210a8425e933e1b06256106311e3d4185d7ffe
kkongmi <kkongmi@192.168.0.109>
2019-01-03 21:18:27 +0900

    StepF modified

--- J_WB/src/workbook/StepF/f_score.java
+++ J_WB/src/workbook/StepF/f_score.java
@@ -6,7 +6,8 @@ public class f_score {
 
 
 	private double[] score= new double[10];
-	private double maxscore, minscore;
+	private double maxscore = score[0];
+	private double minscore = 10.0;
 	private double total, average;
 	private double newnum;
 	
@@ -18,35 +19,24 @@ public class f_score {
 		System.out.printf("%d번 심사점수를 입력하시오. ", i);
 		newnum = s.nextDouble();
 		this.score[i-1] = newnum;
+		for(int j =0; j<i; j++) {
+			if(maxscore<this.score[j]) maxscore = score[j];
+			if(minscore>this.score[j]) minscore = score[j];
 		}
-	}
-	
-	private double getmax() {
-		this.maxscore = this.score[0];
-		for(int i=1; i<10; i++) {
-			if(maxscore<this.score[i]) maxscore = score[i];
 		}
-		return this.maxscore;
-	}
-	
-
-	private double getmin() {
-		this.minscore = this.score[0];
-		for(int i=1; i<10; i++) {
-			if(minscore>this.score[i]) minscore = score[i];
-		}
-		return this.minscore;
+		
 	}
 
 	double gettotal() {
 		for(int i=0; i<10; i++) {
-			if(score[i]!=getmax() && score[i]!=getmin())
+			if(score[i]!=maxscore && score[i]!=minscore)
 			total += score[i];
 		}
 		return this.total;
 	}
+	
 	public void printave() {
-		System.out.printf("%f, %f\n", getmax(), getmin());
+		System.out.printf("%f, %f\n", maxscore, minscore);
 		System.out.printf("가장 큰 수와 가장 작은 점수를 제외한 8개의 점수에 대한 평균은 %.2f입니다.\n", gettotal()/8);
 
 		}
